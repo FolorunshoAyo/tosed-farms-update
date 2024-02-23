cf7c5563cb4c9f7f52f063ecd847fce4d6d36f5e
FolorunshoAyo <folushoayomide11@gmail.com>
2024-02-23 03:08:03 +0100

    Improved the landing page

--- js/agrom.js
+++ js/agrom.js
@@ -268,6 +268,37 @@ $('.team-carousel').slick({
   ]
 });
 
+$('.post-list-carousel').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 //################################
 // ---------- CUSTOMERS ----------
 //################################
